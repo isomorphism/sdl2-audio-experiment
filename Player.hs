6a828ae6af3612378455f632b39de61a1b703449
C. A. McCann <cam@uptoisomorphism.net>
2015-02-28 23:15:54 -0500

    fm synth experiments

--- Player.hs
+++ Player.hs
@@ -29,18 +29,22 @@ import Unsafe.Coerce
 main :: IO ()
 main = do 
     args <- getArgs
-    let bufferSize = 512
-    let (fn1, fn2) = case args of
-                 [f1, f2] -> (f1, f2)
-                 _        -> ("kick.wav", "snare.wav")
+    let bufferSize = 4096
+    let fn1 = "kick.wav"
+    let fn2 = "snare.wav"
+    
+    let fn = case args of
+                 [f1] -> f1
+                 
     
     (info1, Just snd1) <- Snd.readFile fn1 
     (info2, Just snd2) <- Snd.readFile fn2
     
-    --  let song :: BV.Buffer Int32
-        --  song = case s of
-                  --  Nothing -> error "failed to load song"
-                  --  Just x -> x
+    (info3, s) <- Snd.readFile fn
+    let song :: BV.Buffer Int32
+        song = case s of
+                  Nothing -> error "failed to load song"
+                  Just x -> x
     print info1
     print info2
     
@@ -78,9 +82,9 @@ main = do
                         _ -> return Nothing
         drummit _ = return Nothing
     
-    --  let songData = BV.fromBuffer song
-    --  pos <- newIORef 0
-    sounds <- newIORef [] :: IO (IORef [(IORef Int, VS.Vector Int32)])
+    let songData = VS.ifilter (const . even) (BV.fromBuffer song)
+    spos <- newIORef 0
+    sounds <- newIORef [(spos, songData)] :: IO (IORef [(IORef Int, VS.Vector Int32)])
     
     let play i (pos, dat) = do
         p <- readIORef pos
@@ -92,12 +96,32 @@ main = do
           then do return . Just $ VS.slice p (VS.length dat - p) dat <> VS.replicate (fromIntegral i - (VS.length dat - p)) 0
           else return . Just $ VS.slice p (fromIntegral i) dat
     
+    let fm :: Float -> Float -> Float -> Float -> Float -> Float
+        fm c cv m mv = \t -> cv * sin(c' * t + (mv / m') * (sin (m' * t - pi / 2) + 1))
+          where m' = 2 * pi * m
+                c' = 2 * pi * c
+    
+    tref <- newIORef 0
     let callback i = do
-        let smps = fromIntegral i `div` sampleBytes snd1
+        t <- readIORef tref
+        writeIORef tref $ t + i
+        let t' = fromIntegral t `div` 4
+        let tsec x = fromIntegral (x + t') / fromIntegral (Snd.samplerate info1)
         
-        snds <- readIORef sounds
-        out <- catMaybes <$> mapM (play i) snds
-        return . VS.convert . VS.unsafeCast $ (foldr (VS.zipWith (+)) (VS.replicate smps 0) (VS.map (`div` 4) <$> out) :: VS.Vector Int32)
+        let v = VS.generate t' -- (fromIntegral i `div` 4) 
+                    (\x -> round $ (2^29) * sin (tsec x * 4 * 110 + sin (tsec x * 8 * 110 + sin (tsec x * 100 * 110) * (max 0 $ 3 - tsec x/0.5)) * (max 0 $ 2 - tsec x/1)) * (max 0 $ 1 - tsec x/2) :: Int32)
+                    --  (\x -> if (x + t') `mod` 512 > 256 then 2^30 else -2^30 :: Int32)
+                    --  (\x -> if x `mod` 512 > 256 then 2^30 else -2^30 :: Int32)
+                    --  (fm 0.01428 0.5 0.00714 0.2 . (/ fromIntegral (Snd.samplerate info1)) . fromIntegral . (+ fromIntegral t))
+        --  print v
+        --  error "D:"
+        return . VS.convert . VS.unsafeCast $ v
+            
+        --  let smps = fromIntegral i `div` sampleBytes snd1
+        
+        --  snds <- readIORef sounds
+        --  out <- catMaybes <$> mapM (play i) snds
+        --  return . VS.convert . VS.unsafeCast $ (foldr (VS.zipWith (+)) (VS.replicate smps 0) (VS.map (`div` 4) <$> out) :: VS.Vector Int32)
         --  p <- readIORef pos
         --  print (p, i, VS.length songData)
         --  writeIORef pos $ p + fromIntegral i `div` sampleBytes song
@@ -111,8 +135,10 @@ main = do
     
     (adv, _) <- openAudioDevice $ 
         OpenDeviceSpec (Desire . fromIntegral $ Snd.samplerate info1) 
-                       (Desire $ getAudioFormat snd1 (Snd.endianFormat (Snd.format info1) == Snd.EndianBig))
-                       (Desire $ if Snd.channels info1 == 2 then Stereo else Mono)
+                       --  (Desire $ getAudioFormat snd1 (Snd.endianFormat (Snd.format info1) == Snd.EndianBig))
+                       (Desire $ getAudioFormat ([] :: [Int32]) False)
+                       --  (Desire $ if Snd.channels info1 == 2 then Stereo else Mono)
+                       (Desire Mono)
                        bufferSize
                        callback
                        ForPlayback
